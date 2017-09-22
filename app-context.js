ca6e3a2002f5bf7f4b7133227a30676119212b44
Veket <tao.cao@midea.com.cn>
2017-09-22 09:43:45 +0800

    重构后台

--- app-context.js
+++ app-context.js
@@ -5,13 +5,13 @@ var s=module.exports=global.AppCtx={};
 var path=require('path');
 
 s.APP_ROOT_DIR=__dirname;
-s.APP_CONFIG_FILE_PATH='./serve/config/app-config.js';
+s.APP_CONFIG_FILE_PATH='./lib/config/app-config.js';
 s.APP_CONFIG=require(s.APP_CONFIG_FILE_PATH);
 
 s.require=function (p) {
     return require(path.join(s.APP_ROOT_DIR,p));
 };
 
-s.Logger=require('./serve/common/logger.js')(s.APP_CONFIG.LOGGERS);
-s.BaseModel=require('./serve/common/base-model.js');
-s.BaseService=require('./serve/common/base-service.js');
+s.Logger=require('./lib/common/logger.js')(s.APP_CONFIG.LOGGERS);
+s.BaseModel=require('./lib/common/base-model.js');
+s.BaseService=require('./lib/common/base-service.js');
